5a09adcecc5ce2a2adde09f2860e4494b09463fc
lookin-at-the-stars <gabrielmbs051105@gmail.com>
2023-07-20 23:52:53 -0300

    Doctrina 20.07 uma minuscula correção

--- insert-boletim-mensal.php
+++ insert-boletim-mensal.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" type="image/png" href="/Doctrina/icone.png">
+    <link rel="icon" type="image/png" href="/Doctrina/icones.png">
 
     <title>Doctrina- Inicio</title>
 
